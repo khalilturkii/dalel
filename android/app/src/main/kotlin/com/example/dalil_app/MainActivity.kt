e9f2b10665186970ab0c514b0650edb24605647a
khalil <khaliltuki17@gmail.com>
2024-04-24 10:38:07 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/dalil_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dalil_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          color: Colors.blueGrey[200],
+          child: const Center(child: Text("dalel",))),
+    );
+  }
+}
